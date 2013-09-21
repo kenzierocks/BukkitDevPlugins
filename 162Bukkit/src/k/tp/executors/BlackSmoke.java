52f86d0c955a94fabc3ba35b3a020b179e636c31
Kenzie Togami <ket1999@gmail.com>
2013-09-21 12:07:13 -0700

    Made 'default' smoke 'black', and made it thicker

--- 162Bukkit/src/k/tp/executors/BlackSmoke.java
+++ 162Bukkit/src/k/tp/executors/BlackSmoke.java
@@ -10,6 +10,10 @@ public class BlackSmoke extends BaseCE {
 	@Override
 	void preTeleport(Player sp, Location strike1, Location strike2, boolean p2p) {
 		sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
+		sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
+		sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
+		sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
+		sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
 	}
 
 	@Override
@@ -17,6 +21,10 @@ public class BlackSmoke extends BaseCE {
 			Location strike2, boolean p2p, boolean didTele) {
 		if (didTele) {
 			sp.getWorld().playEffect(strike2, Effect.SMOKE, BlockFace.UP);
+			sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
+			sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
+			sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
+			sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
 			sp.sendMessage("Teleported you to " + teleString + " with smoke!");
 		}
 	}
